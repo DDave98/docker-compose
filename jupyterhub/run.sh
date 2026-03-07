36ff57e58dac4b2d446175d52c82c3e2874b33dd
David Michalica <davidmichalica98@email.cz>
2026-03-07 12:50:04 +0100

    Update run.sh

--- jupyterhub/run.sh
+++ jupyterhub/run.sh
@@ -1,1 +1,17 @@
-docker compose up
+#!/bin/bash
+
+echo "Building containers..."
+
+docker compose build
+
+echo "Starting JupyterHub..."
+
+docker compose up -d
+
+echo ""
+echo "Server running:"
+echo "http://localhost:8000"
+echo ""
+echo "Login:"
+echo "username: teacher"
+echo "password: student"
